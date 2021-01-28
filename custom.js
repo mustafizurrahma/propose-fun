fe54aa70cf28fc379abd3778a6defc029c8d8456
mustafizurrahma <58368236+mustafizurrahma@users.noreply.github.com>
2021-01-28 14:52:14 +0600

    Added

---
+++ custom.js
@@ -0,0 +1,11 @@
+const loginButton = document.getElementById("yesOf");
+loginButton.addEventListener("click", function () {
+    const loginArea = document.getElementById("lv-project");
+    loginArea.style.display = "none";
+
+})
+const button = document.getElementById("noNever");
+button.addEventListener("click", function () {
+    const noNever = document.getElementById("lv-project");
+    noNever.style.display = "none";
+})
