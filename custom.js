e7c20bde3b5984338e39e071f2a176354139c6b2
mustafizurrahma <58368236+mustafizurrahma@users.noreply.github.com>
2021-01-28 20:39:23 +0600

    added

--- custom.js
+++ custom.js
@@ -9,3 +9,12 @@ button.addEventListener("click", function () {
     const noNever = document.getElementById("lv-project");
     noNever.style.display = "none";
 })
+
+document.getElementById("yesOf").addEventListener("click", function () {
+    const loveTitleShow = document.getElementById("proposeSection");
+    loveTitleShow.style.display = "block";
+})
+document.getElementById("noNever").addEventListener("click", function () {
+    const loveTitleShow = document.getElementById("noNeverSection");
+    loveTitleShow.style.display = "block";
+})
